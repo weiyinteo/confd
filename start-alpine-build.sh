a4a11ccea694aeba911c21fba65cab1bfa59a378
Wei Yin Teo <wei.teo@algodynamix.com>
2017-02-09 10:40:00 +0000

    Changes: - use golang:1.7-alpine3.5 for building confd

--- start-alpine-build.sh
+++ start-alpine-build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-#docker run -ti --name confd-etcd3 --rm -v "$PWD":/go/src/github.com/kelseyhightower/confd -w /go/src/github.com/kelseyhightower/confd golang:1.6 bash
-docker run -ti --name confd-etcd3 --rm -v "$PWD/../../..":/go/src -w /go/src/github.com/kelseyhightower/confd alpine:3.5 sh
+docker run -ti --name confd-etcd3 --rm -v "$PWD/../../..":/go/src -w /go/src/github.com/kelseyhightower/confd golang:1.7-alpine3.5 go build -v
